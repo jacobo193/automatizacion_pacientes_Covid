aa4c6b1f5d94665fddff7bbe295946587326c566
jacobo193 <60374484+jacobo193@users.noreply.github.com>
2021-05-30 16:30:49 -0500

    Adición de assert
    se adiciono un assert a las pruebas de conseguir listas  de citas por paciente

--- src/test/java/screenplay/steps/CitasDePacienteId.java
+++ src/test/java/screenplay/steps/CitasDePacienteId.java
@@ -1,6 +1,7 @@
 package screenplay.steps;
 
 import co.com.sofkaU.pacientes.test.data.RequestAutenticacion;
+import co.com.sofkaU.pacientes.test.model.JsonCitas;
 import co.com.sofkaU.pacientes.test.task.http.GetRequest;
 import co.com.sofkaU.pacientes.test.task.http.GetRequestWithTokenInvalido;
 import co.com.sofkaU.pacientes.test.task.project.SaveToken;
@@ -9,17 +10,21 @@ import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
 import io.cucumber.java.es.Y;
+import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import net.thucydides.core.util.EnvironmentVariables;
 
 
+import java.util.List;
+
 import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CitasDePacienteId {
     private Actor actor;
     EnvironmentVariables variables;
-    private Integer documento = 642336182;
+    private String documento = "642336182";
     private Integer documentoInvalido = 0;
 
     @Dado(": que un {string} autenticado correctamente en la api")
@@ -48,9 +53,13 @@ public class CitasDePacienteId {
                 seeThatResponse("La api respondera con un estatus 200",
                         response -> response.statusCode(Integer.parseInt(codigo)))
         );
+
+        List<JsonCitas> Citas = SerenityRest.lastResponse().jsonPath().getList("", JsonCitas.class);
+        for (JsonCitas cita : Citas) {
+            assertThat(documento).isEqualTo(String.valueOf(cita.getPatientId()));
+        }
+
     }
-
-
 
     @Cuando(": se introduce un Id de un paciente que no existe")
     public void seIntroduceUnIdDeUnPacienteQueNoExiste() {
